dde86c54cb527cb5400e021d4f27d59f7d476ad1
ljuglaret <ljuglaret@gmail.com>
2022-12-06 20:42:43 +0100

    j6

---
+++ j6/app/src/main/java/j6/J6.java
@@ -0,0 +1,48 @@
+package j6;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class J6 {
+
+    public  String lecture(String fileName) throws IOException {
+        String str="";
+        File file = new File(fileName);
+        FileReader fr = new FileReader(file);
+           
+        try (BufferedReader br = new BufferedReader(fr)) {
+            String line;
+            while((line = br.readLine()) != null){
+                str+=line;
+            }
+            }
+        return str;
+    }
+    public  boolean toutDifferent(String str){
+        for(int i = 0 ; i<  str.length() - 1;i++){
+      for (int j = i + 1 ; j < str.length();j++){
+        if(str.charAt(i) == str.charAt(j)){
+         return false;
+        }
+      }
+    }
+    return true;
+  }
+  public  int p1(String str){
+    int i = 0 ;
+    boolean trouve = false;
+    while(!trouve){
+      String sschaine = str.substring(i,i+4);
+      if(!toutDifferent(sschaine)){
+        i++;
+
+      }
+      else{
+        trouve = true;
+      }
+    }
+    return i+4;
+  }
+    
+}
